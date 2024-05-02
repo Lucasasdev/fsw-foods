ae307816e25fdf3698400f01702efa675fd267c2
Lucas Alves <Lucasasdev@gmail.com>
2024-05-02 10:10:03 -0300

    feat: add promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Header from "./_components/Header";
 import Search from "./_components/Search";
 import CategoryList from "./_components/Category-list";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -16,6 +17,18 @@ const Home = () => {
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas!"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full object-contain"
+          quality={100}
+        />
       </div>
     </>
   );
