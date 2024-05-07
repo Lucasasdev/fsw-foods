47a1c51d89b653e6abad2cf2883c311a7625e262
Lucas Alves <Lucasasdev@gmail.com>
2024-05-07 11:46:02 -0300

    style: update category item styles and add todo

--- app/_components/Restaurant-list.tsx
+++ app/_components/Restaurant-list.tsx
@@ -3,7 +3,7 @@ import RestaurantItem from "./Restaurant-item";
 
 const RestaurantList = async () => {
   const restaurants = await db.restaurant.findMany({ take: 10 });
-
+  // TODO: pegar restaurantes com maior número de pedidos
   return (
     <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {restaurants.map((restaurant) => (
